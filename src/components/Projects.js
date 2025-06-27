817eef55815b26e286107d4ce7c68b43a7424352
Jack See <stjiek@gmail.com>
2025-06-27 22:57:16 +1000

    init9

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,4 @@
-export default function Projects() {
+export default function Commercial Projects() {
   return (
     <section id="projects" style={{ textAlign: 'center' }}>
       <h2 style={{ textAlign: 'center' }}>Projects</h2>
@@ -10,12 +10,14 @@ export default function Projects() {
         <li><h3>&</h3></li>
         <li>
           <h3>- AI Video Style Changer -</h3>
-          <p>[Project_ppt_link] Developed a Video-to-Video AI system for changing the style of a video. Tech used: HuggingFace, Flask, AWS, ...</p>
+          <p>[Project_ppt_link] Founder & CEO | [VideoGenAI.com] – AI-Powered Video Style Transfer Platform
+Dates (e.g., Jan 2022 – Present) Developed a Video-to-Video AI system for changing the style of a video. Tech used: HuggingFace, Flask, AWS, ...</p>
         </li>
         <li><h3>&</h3></li>
         <li>
           <h3>- AI Document Question Answering -</h3>
-          <p>[Project_ppt_link] Developed an AI system for extracting accurate answers from documents in response to user queries. Tech used: HuggingFace, Flask, AWS, ...</p>
+          <p>[Project_ppt_link] Founder & CEO | [VideoGenAI.com] – AI-Powered Video Style Transfer Platform
+Dates (e.g., Jan 2022 – Present) Developed an AI system for extracting accurate answers from documents in response to user queries. Tech used: HuggingFace, Flask, AWS, ...</p>
         </li>
       </ul>
     </section>
